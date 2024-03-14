0adcc1abddf9d00f1b58d02a0610ee95f75c6300
FreemanTheMaverick <63384635+FreemanTheMaverick@users.noreply.github.com>
2024-03-14 16:19:54 +0800

    Update da example

--- examples/da/da.py
+++ examples/da/da.py
@@ -4,6 +4,6 @@ from Orbaplaw import NaturalBondOrbitalMethods as nbo
 mo=wfn.MultiWaveFunction("da.mwfn")
 nao=nbo.NaturalAtomicOrbital(mo)
 nao.Export("da_nao.mwfn")
-nho,nbo=nbo.NaturalBondOrbital(nao,frags=[[i for i in range(26)],[26,27,28,29,30,31]])
+nho,nbo=nbo.NaturalBondOrbital(nao,frags=[[i for i in range(26)],[26,27,28,29,30,31]],threshold=0.1)
 nho.Export("da_nfho.mwfn")
 nbo.Export("da_nfbo.mwfn")
