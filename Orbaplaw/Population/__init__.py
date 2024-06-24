d58843299844d718c75dcbaebfa3342a54735186
FreemanTheMaverick <63384635+FreemanTheMaverick@users.noreply.github.com>
2024-06-24 16:53:40 +0800

    Lowdin population analysis and derivative

---
+++ Orbaplaw/Population/__init__.py
@@ -0,0 +1,6 @@
+from Orbaplaw.Population import Lowdin
+Lowdin_func=Lowdin.Lowdin_func
+Lowdin_jac=Lowdin.Lowdin_jac
+
+from Orbaplaw.Population import Analyzer
+Analyzer=Analyzer.Analyzer
--- Orbaplaw/__init__.py
+++ Orbaplaw/__init__.py
@@ -1,3 +1,4 @@
 from Orbaplaw import WaveFunction
+from Orbaplaw import Population
 from Orbaplaw import Localization
 from Orbaplaw import NaturalBondOrbitalMethods
