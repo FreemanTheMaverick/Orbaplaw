0182da4c4b11dd36a62aef3d561bcb148a55791c
FreemanTheMaverick <63384635+FreemanTheMaverick@users.noreply.github.com>
2024-07-03 18:33:49 +0800

    Delete __init__.py
    which is misplaced

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from Orbaplaw import WaveFunction
-from Orbaplaw import Population
-from Orbaplaw import Localization
-from Orbaplaw import NaturalBondOrbitalMethods
-from Orbaplaw import Miscellany
