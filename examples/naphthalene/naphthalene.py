f3edc52f2863ac4f1913f410fe4b7b6a88d8ca26
Yichi ZHANG <freemanz771746166@163.com>
2025-01-05 01:54:43 +0800

    Removed irrelevant examples

--- examples/naphthalene/naphthalene.py
+++
@@ -1,9 +0,0 @@
-from Orbaplaw import WaveFunction as wfn
-from Orbaplaw import NaturalBondOrbitalMethods as nbo
-
-mo=wfn.MultiWaveFunction("naphthalene.mwfn")
-nao=nbo.NaturalAtomicOrbital(mo)
-nao.Export("naphthalene_nao.mwfn")
-nho,nbo=nbo.NaturalBondOrbital(nao,frags=[[i for i in range(10)],[i for i in range(10,20)],[20,21],[22,23]],threshold=0.05)
-nho.Export("naphthalene_nfho.mwfn")
-nbo.Export("naphthalene_nfbo.mwfn")
