aaaac4e08ffcd9fbd5271ec3b5350a8acae7bddf
Yichi ZHANG <freemanz771746166@163.com>
2025-01-05 16:44:15 +0800

    Updated Zeise salt example.
    This is quite tricky and needs further discussion.

--- examples/zeise/zeise.py
+++ examples/zeise/zeise.py
@@ -1,9 +1,16 @@
 from Orbaplaw import WaveFunction as wfn
 from Orbaplaw import NaturalBondOrbitalMethods as nbo
 
-mo=wfn.MultiWaveFunction("zeise.mwfn")
-nao=nbo.NaturalAtomicOrbital(mo)
-nao.Export("zeise_nao.mwfn")
-nho,nbo=nbo.NaturalBondOrbital(nao,frags=[[0],[1],[2],[3],[4,5,6,7,8,9]],threshold=0.05)
-nho.Export("zeise_nfho.mwfn")
-nbo.Export("zeise_nfbo.mwfn")
+mo = wfn.MultiWaveFunction("zeise.mwfn")
+print("The three-center-four-electron scheme:")
+nao = nbo.NaturalAtomicOrbital(mo)
+nao.Export("zeise_3c4e_nao.mwfn")
+nfho, nfbo = nbo.NaturalBondOrbital(nao, frags = [ [0], [1], [2], [3], [4, 5, 6, 7, 8, 9] ], multi_thres = 0.90)
+nfho.Export("zeise_3c4e_nfho.mwfn")
+nfbo.Export("zeise_3c4e_nfbo.mwfn")
+print("The hybridized-orbital scheme:")
+nao = nbo.NaturalAtomicOrbital(mo,modify_minimal_shells=[(0,[9,9,9,9,9,9,9])])
+nao.Export("zeise_hybridized_nao.mwfn")
+nfho, nfbo = nbo.NaturalBondOrbital(nao, frags = [ [0], [1], [2], [3], [4, 5, 6, 7, 8, 9] ], multi_thres = 0.95, deg_thres = 0.05)
+nfho.Export("zeise_hybridized_nfho.mwfn")
+nfbo.Export("zeise_hybridized_nfbo.mwfn")
