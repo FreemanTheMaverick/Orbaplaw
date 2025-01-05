eee28f6fc49578ea7243759dfab9b78f3391e7dd
Yichi ZHANG <freemanz771746166@163.com>
2025-01-05 18:50:00 +0800

    A new example involving spin-unrestricted wavefunction

--- examples/tbu/tbu.py
+++ examples/tbu/tbu.py
@@ -1,9 +1,18 @@
 from Orbaplaw import WaveFunction as wfn
 from Orbaplaw import NaturalBondOrbitalMethods as nbo
 
-mo=wfn.MultiWaveFunction("tbu.mwfn")
-nao=nbo.NaturalAtomicOrbital(mo)
+mo = wfn.MultiWaveFunction("tbu.mwfn")
+nao = nbo.NaturalAtomicOrbital(mo)
 nao.Export("tbu_nao.mwfn")
-pio,pimo=nbo.PrincipalInteractingOrbital(nao,[[1,2,3,4],[0]+[i for i in range(5,13)]])
+nfho, nfbo = nbo.NaturalBondOrbital(nao, frags = [
+	[1, 2, 3, 4],
+	[0] + [i for i in range(5, 13)]
+])
+nfho.Export("tbu_nfho.mwfn")
+nfbo.Export("tbu_nfbo.mwfn")
+pio, pimo = nbo.PrincipalInteractingOrbital(nao, [
+	[1, 2, 3, 4],
+	[0] + [i for i in range(5, 13)]
+])
 pio.Export("tbu_pio.mwfn")
 pimo.Export("tbu_pimo.mwfn")
