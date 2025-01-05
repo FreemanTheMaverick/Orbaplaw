ae77628ab8de154c381ca9dc7f27b9203e3604ef
Yichi ZHANG <freemanz771746166@163.com>
2025-01-06 00:08:41 +0800

    Updated the onion example
    The three-layer model is not good for NFBO analysis. The outer two layers are too close to be differentiated.

--- examples/onion/onion.py
+++ examples/onion/onion.py
@@ -1,9 +1,17 @@
 from Orbaplaw import WaveFunction as wfn
 from Orbaplaw import NaturalBondOrbitalMethods as nbo
 
-mo=wfn.MultiWaveFunction("onion.mwfn")
-nao=nbo.NaturalAtomicOrbital(mo)
-nho,nbo=nbo.NaturalBondOrbital(nao,frags=[[0],[i for i in range(1,13)],[i for i in range(13,33)]],threshold=0.025)
-nho.Export("onion_nfho.mwfn")
-nbo.Export("onion_nfbo.mwfn")
-
+mo = wfn.MultiWaveFunction("onion.mwfn")
+nao = nbo.NaturalAtomicOrbital(mo)
+nfho, nfbo = nbo.NaturalBondOrbital(nao, frags = [
+	[0],
+	[i for i in range(1, 33)],
+])
+nfho.Export("onion_nfho.mwfn")
+nfbo.Export("onion_nfbo.mwfn")
+pio, pimo = nbo.PrincipalInteractingOrbital(nao, [
+	[0],
+	[i for i in range(1, 33)],
+])
+pio.Export("onion_pio.mwfn")
+pimo.Export("onion_pimo.mwfn")
