c20fac15d093809b69cc4c20003df9cda75467d5
Yichi ZHANG <freemanz771746166@163.com>
2025-04-10 01:48:31 +0800

    Localized orbitalet supported

--- Orbaplaw/Localization/Fock.py
+++ Orbaplaw/Localization/Fock.py
@@ -1,57 +1,38 @@
 import numpy as np
-import scipy.linalg as sl
 import Maniverse as mv
-from Orbaplaw import Optimization as opt
 
-def Fock_func(U,C0,F,F2):
-	C=C0@U
-	return np.sum(np.diag(C.T@F2@C)-np.diag(C.T@F@C)**2)
-
-def Fock_jac(U,C0,F,F2):
-	C=C0@U
-	Gamma=C0.T@(2*(F2@C)-4*(F@C)*np.diag(C.T@F@C)) # Euclidean derivative
-	return Gamma@U.T-U@Gamma.T # Riemannian derivative
-
-def Fock_conv(x,f,g,xlast,flast,glast):
-	return np.max(np.abs(g))<5e-2 and abs(f-flast)<1e-4
-
-def Fock(C0,S,F,conv):
-	epsilon,Cmo=sl.eigh(F,b=S)
-	a=S@(Cmo*epsilon)
-	F2=a@a.T
-	func=lambda x:-Fock_func(x,C0,F,F2)
-	jac=lambda x:-Fock_jac(x,C0,F,F2)
-	conv_=Fock_conv if conv is None else conv
-	return opt.Lehtola(C0,0,func,jac,4,conv_)
-
-'''
-def Fock(C, E, S):
-	Fref = C.T @ S @ C @ np.diag(E) @ C.T @ S @ C
-	F2ref = C.T @ S @ C @ np.diag(E)**2 @ C.T @ S @ C
-	M = mv.Orthogonal(np.eye(Fref.shape[0]), True)
-	def func(U, order):
-		F = U.T @ Fref @ U
-		F2 = U.T @ F2ref @ U
-		L = np.trace(F2) - np.linalg.norm(np.diag(F)) ** 2;
-		Fdiag = np.diag(np.diag(F))
-		Ge = 2 @ F2ref @ U - 4 * Fref @ U @ Fdiag
-		twoF2ref = 2 * F2ref
-		fourFref = 4 * Fref
-		eightFrefU = 8 * Fref @ U
-		UTFref = U.T @ Fref
+def Fock_func(U, order, Eref):
+	Fref = np.diag(Eref)
+	F2ref = np.diag(Eref**2)
+	F = U.T @ Fref @ U
+	DiagF = np.diag(np.diag(F))
+	F2 = U.T @ F2ref @ U
+	L = np.trace(F2) - np.linalg.norm(DiagF) ** 2
+	Ge = 2 * F2ref @ U - 4 * Fref @ U @ DiagF
+	twoF2ref = 2 * F2ref
+	fourFref = 4 * Fref
+	eightFrefU = 8 * Fref @ U
+	UTFref = U.T @ Fref
+	def He(v):
+		return v
+	if order == 2:
 		def He(v):
-			return v
-		if order == 2:
-			def He(v):
-				return twoF2ref @ v
-					- fourFref @ v @ Fdiag
-					- eightFrefU @ np.diag(np.diag(UTFref @ v))
+			Hv = twoF2ref @ v - fourFref @ v @ DiagF - eightFrefU @ np.diag(np.diag(UTFref @ v))
+			return Hv
+	return L, Ge, He
+
+def Fock(Eref):
+	M = mv.Orthogonal(np.eye(len(Eref)), True)
+	def func(U, order):
+		L, Ge, He = Fock_func(U, order, Eref)
 		return L, Ge, He
 	L = 0
 	tr_setting = mv.TrustRegionSetting()
+	tol0 = 1e-8 * M.getDimension()
+	tol1 = 1e-6 * M.getDimension()
+	tol2 = 10
 	mv.TrustRegion(
-			func, tr_setting, (1.e-6, 1.e-4, 1.e-7),
-			1, 100, L, M, True
+			func, tr_setting, (tol0, tol1, tol2),
+			0.001, 1, 1000, L, M, 1
 	)
 	return M.P
-	'''
--- Orbaplaw/Localization/Orbitalet.py
+++ Orbaplaw/Localization/Orbitalet.py
@@ -1,18 +1,27 @@
 import numpy as np
-import scipy.linalg as sl
-from . import FB_func, FB_jac
-from . import Fock_func, Fock_jac
-from Orbaplaw import Optimization as opt
+import Maniverse as mv
+from . import FB_func
+from . import Fock_func
 
-
-def Orbitalet_conv(x,f,g,xlast,flast,glast):
-    return np.max(np.abs(g))<5e-2 and abs(f-flast)<1e-4
-
-def Orbitalet(C0,Ws,W2s,S,F,gamma,CC,conv):
-    epsilon,Cmo=sl.eigh(F,b=S)
-    a=S@(Cmo*epsilon)
-    F2=a@a.T
-    func=lambda x:-(1.-gamma)*FB_func(x,C0,Ws,W2s)-gamma*CC*Fock_func(x,C0,F,F2)
-    jac=lambda x:-(1.-gamma)*FB_jac(x,C0,Ws,W2s)-gamma*CC*Fock_jac(x,C0,F,F2)
-    conv_=Orbitalet_conv if conv is None else conv
-    return opt.Lehtola(C0,0,func,jac,4,conv_)
+def Orbitalet(Wrefs, W2refSum, Eref, gamma_e):
+	M = mv.Orthogonal(np.eye(len(Eref)), True)
+	Scale1 = 1. - gamma_e
+	Scale2 = gamma_e * 1000
+	def func(U, order):
+		L1, Ge1, He1 = FB_func(U, order, Wrefs, W2refSum)
+		L2, Ge2, He2 = Fock_func(U, order, Eref)
+		L = Scale1 * L1 + Scale2 * L2
+		Ge = Scale1 * Ge1 + Scale2 * Ge2
+		def He(v):
+			return Scale1 * He1(v) + Scale2 * He2(v)
+		return L, Ge, He
+	L = 0
+	tr_setting = mv.TrustRegionSetting()
+	tol0 = 1e-8 * M.getDimension()
+	tol1 = 1e-6 * M.getDimension()
+	tol2 = 10
+	mv.TrustRegion(
+			func, tr_setting, (tol0, tol1, tol2),
+			0.001, 1, 1000, L, M, 1
+	)
+	return M.P
