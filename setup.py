006ec8850295b093320aa09ff730a1912dd5610c
FreemanTheMaverick <freemanz771746166@163.com>
2025-05-26 16:30:06 +0800

    Moved long description to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 __version__ = "1.0.2"
 
+with open("README.md", 'r') as f:
+	long_description = f.read()
+
 setup(
 		name = "Orbaplaw",
 		version = __version__,
 		author = "FreemanTheMaverick",
 		description = "Orbital alignment analysis for plane wave basis sets",
+		long_description = long_description,
 		packages = find_packages(),
 		url = "https://github.com/FreemanTheMaverick/Orbaplaw.git",
 		install_requires = ["numpy", "scipy", "pyscf", "Maniverse"],
