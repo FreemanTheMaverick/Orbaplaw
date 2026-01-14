5388220842094f27d773e7668f2d476d8664586a
FreemanTheMaverick <freemanz771746166@163.com>
2026-01-15 00:08:53 +0800

    Adapted for Maniverse-2.0.0

--- Orbaplaw/Localization/Fock.py
+++ Orbaplaw/Localization/Fock.py
@@ -1,39 +1,40 @@
 import numpy as np
 import Maniverse as mv
 
-def Fock_func(U, order, Eref):
-	Fref = np.diag(Eref)
-	F2ref = np.diag(Eref**2)
-	F = U.T @ Fref @ U
-	DiagF = np.diag(np.diag(F))
-	F2 = U.T @ F2ref @ U
-	L = np.trace(F2) - np.linalg.norm(DiagF) ** 2
-	Ge = 2 * F2ref @ U - 4 * Fref @ U @ DiagF
-	twoF2ref = 2 * F2ref
-	fourFref = 4 * Fref
-	eightFrefU = 8 * Fref @ U
-	UTFref = U.T @ Fref
-	def He(v):
-		return v
-	if order == 2:
-		def He(v):
-			Hv = twoF2ref @ v - fourFref @ v @ DiagF - eightFrefU @ np.diag(np.diag(UTFref @ v))
-			return Hv
-	return L, Ge, He
+class FockObj(mv.Objective):
+	def __init__(self, Eref):
+		super().__init__()
+		self.Fref = np.diag(Eref)
+		self.F2ref = np.diag(Eref ** 2)
+		self.DiagF = None
+		self.twoF2ref = None
+		self.fourFref = None
+		self.eightFrefU = None
+		self.UTFref = None
+
+	def Calculate(self, U, _):
+		F = U[0].T @ self.Fref @ U[0]
+		F2 = U[0].T @ self.F2ref @ U[0]
+		self.DiagF = np.diag(np.diag(F))
+		self.Value = np.trace(F2) - np.linalg.norm(self.DiagF) ** 2
+		self.Gradient = [ 2 * self.F2ref @ U[0] - 4 * self.Fref @ U[0] @ self.DiagF ]
+		self.twoF2ref = 2 * self.F2ref
+		self.fourFref = 4 * self.Fref
+		self.eightFrefU = 8 * self.Fref @ U[0]
+		self.UTFref = U[0].T @ self.Fref
+
+	def Hessian(self, V):
+		return [[ self.twoF2ref @ V[0] - self.fourFref @ V[0] @ self.DiagF - self.eightFrefU @ np.diag(np.diag(self.UTFref @ V[0])) ]]
 
 def Fock(Eref):
-	M = mv.Iterate([mv.Orthogonal(np.eye(len(Eref)))], True)
-	def func(Us, order):
-		U = Us[0]
-		L, Ge, He = Fock_func(U, order, Eref)
-		return L, [Ge], [He]
-	L = 0
-	tr_setting = mv.TrustRegionSetting()
+	obj = FockObj(Eref)
+	M = mv.Iterate(obj, [mv.Orthogonal(np.eye(len(Eref)))], True)
+	tr_setting = mv.TrustRegion()
 	tol0 = 1e-8 * M.getDimension()
 	tol1 = 1e-6 * M.getDimension()
 	tol2 = 10
-	mv.TrustRegion(
-			func, tr_setting, (tol0, tol1, tol2),
-			0.001, 1, 1000, L, M, 1
+	mv.TruncatedNewton(
+			M, tr_setting, (tol0, tol1, tol2),
+			0.001, 1000, 1
 	)
 	return M.Point
